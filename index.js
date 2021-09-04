7a5d73df54176ff0bc3838158c61ed5c141275c2
akashartist001 <89149894+akashartist001@users.noreply.github.com>
2021-09-04 13:45:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,32 @@
+
+//getting img1 ,img2 , button
 var image1 = document.getElementsByClassName("img1")[0];
 var image2 = document.getElementsByClassName("img2")[0];
+var rollButton = document.querySelector("button");
 
-function diceRoll() {
-  image1.classList.add("rotate");
-  image2.classList.add("rotate");
+rollButton.addEventListener("click",diceRoll);
+//function to roll dice
+ function diceRoll(){
+   //adding rotate animation
+   image1.classList.add("rotate");
+   image2.classList.add("rotate");
+
+   //chnaging image to show value 1 to 6 for image1
+  var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+  var randomImageSource1 = "images/dice"+randomNumber1+".png";
+  image1.setAttribute("src" , randomImageSource1);
+ //chnaging image to show value 1 to 6 for image2
+  var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+  var randomImageSource2 = "images/dice"+randomNumber2+".png";
+  image2.setAttribute("src" , randomImageSource2);
+
+/*providing dealy so that class .rotate get added and get removed after 300 miliecond
+so our dice will rotate every time button get clicked*/
+  setTimeout(function(){
+    image1.classList.remove("rotate");
+    image2.classList.remove("rotate");
+  },300);
 
 
-  switch (Math.floor(Math.random() * 6 + 1)) {
-    case 1:
-      image1.src = "images/dice1.png";
-      break;
-    case 2:
-      image1.src = "images/dice2.png";
-      break;
-    case 3:
-      image1.src = "images/dice3.png";
-      break;
-    case 4:
-      image1.src = "images/dice4.png";
-      break;
-    case 5:
-      image1.src = "images/dice5.png";
-      break;
-    case 6:
-      image1.src = "images/dice6.png";
-      break;
-    default:
 
-  }
-
-  switch (Math.floor(Math.random() * 6 + 1)) {
-    case 1:
-      image2.src = "images/dice1.png";
-      break;
-    case 2:
-      image2.src = "images/dice2.png";
-      break;
-    case 3:
-      image2.src = "images/dice3.png";
-      break;
-    case 4:
-      image2.src = "images/dice4.png";
-      break;
-    case 5:
-      image2.src = "images/dice5.png";
-      break;
-    case 6:
-      image2.src = "images/dice6.png";
-      break;
-    default:
-
-  }
-}
-
-function refreshRoll() {
-  image1.classList.remove("rotate");
-  image2.classList.remove("rotate");
 }
